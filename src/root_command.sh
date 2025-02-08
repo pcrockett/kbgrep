3cc866baa50a2b9edc640540f418a23be7ea4e6d
Phil Crockett <contact@philcrockett.com>
2025-02-08 13:37:45 +0100

    fix(bat): remove unnecessary decorations
    features like line numbers were taking up too much screen real estate,
    and i don't need them

--- src/root_command.sh
+++ src/root_command.sh
@@ -14,8 +14,8 @@ if [ "${#args[@]}" -eq 0 ] && [ "${#other_args[@]}" -eq 0 ] && ! is_interactive;
             --disabled \
             --bind "change:reload(echo {q} | xargs ${kbg_command} || true)" \
             --bind "ctrl-r:reload(echo {q} | xargs ${kbg_command} || true)" \
-            --bind "ctrl-d:execute(bat --color=always --wrap never --pager '${bat_pager}' {})" \
-            --preview "bat --color=always --terminal-width \${FZF_PREVIEW_COLUMNS} --wrap never --paging never {}" \
+            --bind "ctrl-d:execute(bat --color=always --wrap never --pager '${bat_pager}' --plain {})" \
+            --preview "bat --color=always --terminal-width \${FZF_PREVIEW_COLUMNS} --wrap never --paging never --plain {}" \
             --preview-window 'up,70%,border-bottom,+{2}+3/3,~3' \
             --bind "enter:become(${search_result_command} {})"
 
